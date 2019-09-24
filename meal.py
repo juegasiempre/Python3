0864a85ea029a2a7e9614b2b479ccf1085363064
juegasiempre <42561121+juegasiempre@users.noreply.github.com>
2019-09-24 15:41:49 +1000

    Add files via upload

--- meal.py
+++ meal.py
@@ -2,8 +2,11 @@ import random
 import readline
 import smtplib
 import ssl
+import time
 
-"""Weekly shopping list and meal generator v0.01 with random choice generation for lazy mem-breads."""
+"""Weekly shopping list and meal generator v0.01 with random choice generation for mem-breads/membrillo.
+Change log:
+v0.02 @ 24092018 - Amended recipes, added shopping list checker and updated email template """
 
 nights = ['Sunday night','Monday night','Tuesday night','Wednsday night','Thursday night','Friday night']
 meals = {1:'Lasagna',2:'Chilli con carne', 3:'Burgers', 4:'Butter chicken', 5:'Pizza', 6:'Thai curry', 7:'Drumsticks', 8:'Shepards pie', 9:'Dal', 10:'Carnitas', 11:'Cabonara', 12:'Risotto', 13:'Wraps'}
@@ -13,8 +16,12 @@ ingredients = {'Mince':0,'Olive oil':0,'Onion':0,'Garlic':0,'Cumin':0,'Chilli po
 ,'Pasta':0,'Cream':0,'Eggs':0,'Bacon':0,'Parmesan cheese':0,'Mushrooms':0,'White wine':0,'Risotto Rice':0,'Chicken stock':0}
 
 recipes = {'Chilli con carne':{'Mince':680,'Olive oil':45,'Garlic':30,'Cumin':10,'Chilli powder':15,'Oregano':0,'Salt':7,'Pepper':2,'Worcestershire sauce':15,'Tomato cans':2,'Black beans':1,'Red beans':1
-,'Fish sauce':15,'Cocoa powder':10,'Corn chips': 1},'Dal':{'Onion':2,'Tomato cans':2,'Chilli':2,'Ginger':10,'Black mustard seeds':5,'Cumin seeds':5,'Tumeric':5,'Coriander powder':5,'Butter':90,'Coriander leaves':10},'Lasagna':{'Mince':500,'Olive oil':30,'Onion':1,'Garlic':2,'Chilli powder':5,'Oregano':5,'Salt':2,'Pepper':2,'Tomato cans':2,'Lasagna sheets':9,'Tomato Paste':30},'Burgers':{'Mince':500,'Salt':7,'Pepper':2,'Bread rolls':6},'Butter chicken':{'Chicken':500, 'Rice':300,'Butter chicken paste':50,'Cashew/peanut':50, 'Greek yoghurt':50,'Butter':50},'Pizza':{'White flour':500, 'Salami':10, 'Mozzerella':200,'Yeast':5,'Salt':7,'Basil':5,'Tomato cans':1,'Tomato Paste':50},'Thai curry':{'Chicken':500, 'Rice':300,'Thai curry paste':50,'Coconut cream can':1,'Frozen veg':200,'Chilli':2},'Drumsticks':{'Chicken Drumsticks':8,'Honey':30,'Soy sauce':30,'Rice': 300,'Chilli':2},'Wraps':{'Wraps/Pita':6,'Salad':100,'Tuna cans':2,'Cheese':50},'Shepards pie':{'Mince':500,'Olive oil':15,'Onion':1,'Tomato Paste':15,'Salt':2,'Pepper':2,'White flour':30,'Beef stock':500,'Worcestershire sauce':15,'Potatoes':4,'Butter':40,'Milk':125},'Carnitas':{'Pork roast':1000,'Garlic':30,'Lemon juice':200,'Oregano':10,'Salt':5,'Pepper':5,'Cumin':5,'Tortillas':4},'Cabonara':{'Pasta':400,'Cream':100,'Eggs':4,'Bacon':200,'Parmesan cheese':50,'Garlic':2,'Olive oil':30},'Risotto':{'Butter':60, 'Parmesan cheese':60, 'Mushrooms':300,'White wine':100,'Risotto Rice':300,'Bacon':100,'Garlic':1,'Onion':1,'Chicken':400,'Olive oil':60, 'Chicken stock':800}}
+,'Fish sauce':15,'Cocoa powder':10,'Corn chips': 1},'Dal':{'Onion':2,'Tomato cans':2,'Chilli':2,'Ginger':10,'Black mustard seeds':5,'Cumin seeds':5,'Tumeric':5,'Coriander powder':5,'Butter':90,'Coriander leaves':10},'Lasagna':{'Mince':500,'Olive oil':30,'Onion':1,'Garlic':2,'Chilli powder':5,'Oregano':5,'Salt':2,'Pepper':2,'Tomato cans':2,'Lasagna sheets':9,'Tomato Paste':30},'Burgers':{'Mince':500,'Salt':7,'Pepper':2,'Bread rolls':6},'Butter chicken':{'Chicken':1000, 'Rice':300,'Butter chicken paste':50,'Cashew/peanut':50, 'Greek yoghurt':100,'Butter':50,'Cream':200},'Pizza':{'White flour':450, 'Salami':10, 'Mozzerella':200,'Yeast':5,'Salt':7,'Basil':5,'Tomato cans':1,'Tomato Paste':50},'Thai curry':{'Chicken':500, 'Rice':300,'Thai curry paste':50,'Coconut cream can':1,'Frozen veg':200,'Chilli':2},'Drumsticks':{'Chicken Drumsticks':8,'Honey':30,'Soy sauce':30,'Rice': 300,'Chilli':2},'Wraps':{'Wraps/Pita':6,'Salad':100,'Tuna cans':2,'Cheese':50},'Shepards pie':{'Mince':500,'Olive oil':15,'Onion':1,'Tomato Paste':15,'Salt':2,'Pepper':2,'White flour':30,'Beef stock':500,'Worcestershire sauce':15,'Potatoes':4,'Butter':40,'Milk':125},'Carnitas':{'Pork roast':1000,'Garlic':30,'Lemon juice':200,'Oregano':10,'Salt':5,'Pepper':5,'Cumin':5,'Tortillas':4},'Cabonara':{'Pasta':400,'Cream':100,'Eggs':4,'Bacon':200,'Parmesan cheese':50,'Garlic':2,'Olive oil':30},'Risotto':{'Butter':60, 'Parmesan cheese':60, 'Mushrooms':300,'White wine':100,'Risotto Rice':300,'Bacon':100,'Garlic':1,'Onion':2,'Chicken':400,'Olive oil':60, 'Chicken stock':800}}
 
+
+def neal_faq():
+	faq = ['Neal was born in Gympie.  This is known as Gtown to the locals or Massive POS town to the others','Neal works 60 hours a week to help his girlfriend and future family, what a top bloke.','Neal likes to Surf and Skate.  One day he hopes to live in Nicaragua.','Neal has been to over 30 countries!',"Neal's favourite macronutrient is Protein, yum!", 'Neal enjoys the music of Bob Marley and has visited his home in Kingston, Jamaica.  Wow!','Neal has been learning Python since 2017!','Neal likes to garden','Neal has lots of cool stories, feel free to ask him to share one!']
+	return 'Fun fact about Neal: {}'.format(faq[random.randint(0,len(faq)-1)])
 
 def email():
     """This module will email Membrillo the shopping list"""
@@ -27,7 +34,7 @@ def email():
     server.starttls()
     server.login(sender_email,password)
     try:
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(sender_email, receiver_email, finalmessage)
         print('Email sent!')
     except:
         print('Error :( ')
@@ -35,43 +42,65 @@ def email():
 	
 	
 choice = ''
-message = ''
 
 while choice.lower() != 'yes':
-        selection = []
-        rand_no = input('Do you want to choose the meal plan?  ')
-        print('\n')
-        if rand_no.lower() == 'yes' or  rand_no.lower() == 'y':
-                for k,v in meals.items():
-                        print('Press {} for {} '.format(k,v))
-                print('\n')
-                for x in range(len(nights)):
-                        print('Enter your selection for {}: \n'.format(nights[x]))
-                        temp = int(input())
-                        if type(temp) != int:
-                                print('Enter the number of the meal you want!')
-                                temp = int(input())
-                        selection.append(meals[temp])
-        else:
-                while len(selection) != len(nights):
-                        robot_choice = meals[random.randint(1,len(meals))]
-                        if robot_choice not in selection:
-                                selection.append(robot_choice)
+	finalmessage = ''
+	selection = []
+	rand_no = input('Do you want to choose the meal plan?  ')
+	print('\n')
+	if rand_no.lower() == 'yes' or  rand_no.lower() == 'y':
+		for k,v in meals.items():
+			print('Press {} for {} '.format(k,v))
+		print('\n')
+		for x in range(len(nights)):
+			print('Enter your selection for {}: '.format(nights[x]))
+			temp = int(input())
+			if type(temp) != int:
+				print('Enter the number of the meal you want!')
+				temp = int(input())
+			selection.append(meals[temp])
+	else:
+		while len(selection) != len(nights):
+			robot_choice = meals[random.randint(1,len(meals))]
+			if robot_choice not in selection:
+				selection.append(robot_choice)
+	print('\nThe meals for the week are: \n')
 
-        print('The meals for the week are: \n')
-        for x in range(len(nights)):
-                print("{} we're having {}".format(nights[x],selection[x]))
-        choice = input("Is this correct? (Type 'yes' or 'no') \n")
+	for x in range(len(nights)):
+		print("{} we're having {}".format(nights[x],selection[x]))
+		finalmessage += "{} we're having {} \n".format(nights[x],selection[x])
+	print('\n')
+	choice = input("Is this correct? (Type 'yes' or 'no') \n")
 
-
-print("For the following week, you'll need:\n")
+ingredient_selection = []
+choice1 = ''
 for x in selection:
-        for k,v in recipes[x].items():
-                ingredients[k] += v
-for k,v in ingredients.items():
-	if v != 0:
-            message += "{} grams/ml/units of {}\n".format(v,k)
-            print("{} grams/ml/units of {}".format(v,k))
+	for k,v in recipes[x].items():
+        	ingredients[k] += v
+	for k,v in ingredients.items():
+		if v != 0:
+			if k not in ingredient_selection:
+				ingredient_selection.append(k)
 
-print('Sending email maybe')
-email()		
+
+while choice1.lower() != 'yes':
+	final = []
+	print('\n')
+	for x in range(len(ingredient_selection)):
+		doweneed = input("Do we need {} (Type 'yes' or 'y' for yes, anything else for no)? ".format(ingredient_selection[x]))
+		if doweneed == 'y' or doweneed == 'yes':
+			final.append(ingredient_selection[x])
+	finalmessage += '\n'
+	finalmessage += 'For the following week we need to buy: \n'
+	for x in final:
+		finalmessage += x+'\n'
+	time.sleep(2)
+	print('\nThe shopping list for this week is:')
+	for x in final:
+		print(x)
+	print('\n')
+	choice1 = input("Is this correct (Type 'yes' for yes, anything else for no)? ")
+finalmessage += '\n'+neal_faq()
+print('Robot powering up and crafting unique email specifically for you......NOW!')
+#email()
+print(finalmessage)
